75c63877077352d6cfa0757ae896ff20b1837dc7
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-06 13:29:26 -0400

    Rearrange containers for settings button

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 5 Aug 2024, 7:08:18 PM
- *  Last update: 6 Aug 2024, 1:22:00 PM
+ *  Last update: 6 Aug 2024, 1:28:42 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { View, Text, Image } from "react-native";
@@ -12,32 +12,37 @@ import styles from "./styles";
 export default function HomeScreen() {
     return (
         <View
-            style={[ globalStyles.navContainer, styles.container ]}
+            style={[ globalStyles.navContainer, styles.outerContainer ]}
         >
-            <View style={styles.titleContainer}>
-                <Image
-                    source={require("../../../assets/logo.png")}
-                    resizeMode="contain"
-                    style={styles.logo}
-                />
-                <Text style={styles.subtitle}>Calculator</Text>
-            </View>
-
-            <View style={styles.introContainer}>
-                <Text style={[ styles.introText, globalStyles.titleH1 ]}>
-                    Welcome!
-                </Text>
-                <View style={styles.introBodyContainer}>
-                    <Text style={styles.introText}>
-                        This tool is a revenue calculator for Stardew Valley.
-                    </Text>
-                    <Text style={styles.introText}>
-                        Get started by heading to the items tab and select some items to add to the calculator.
-                    </Text>
-                    <Text style={styles.introText}>
-                        Then, switch to the calculator tab and modify the quantities to see your revenue totals, broken down by day and season!
-                    </Text>
+            <View style={styles.topContainer}>
+                <View style={styles.titleContainer}>
+                    <Image
+                        source={require("../../../assets/logo.png")}
+                        resizeMode="contain"
+                        style={styles.logo}
+                    />
+                    <Text style={styles.subtitle}>Calculator</Text>
                 </View>
+
+                <View style={styles.introContainer}>
+                    <Text style={[ styles.introText, globalStyles.titleH1 ]}>
+                        Welcome!
+                    </Text>
+                    <View style={styles.introBodyContainer}>
+                        <Text style={styles.introText}>
+                            This tool is a revenue calculator for Stardew Valley.
+                        </Text>
+                        <Text style={styles.introText}>
+                            Get started by heading to the items tab and select some items to add to the calculator.
+                        </Text>
+                        <Text style={styles.introText}>
+                            Then, switch to the calculator tab and modify the quantities to see your revenue totals, broken down by day and season!
+                        </Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.bottomContainer}>
+                <Text>Test</Text>
             </View>
         </View>
     );
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Aug 2024, 12:28:21 PM
- *  Last update: 6 Aug 2024, 1:21:59 PM
+ *  Last update: 6 Aug 2024, 1:28:34 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StyleSheet } from "react-native";
@@ -12,9 +12,18 @@ import {
 } from "../../styles/structure";
 
 export default StyleSheet.create({
-    container: {
+    outerContainer: {
         flex: 1,
         gap: gapPadding,
+        justifyContent: "space-between",
+    },
+
+    topContainer: {
+        gap: gapPadding,
+    },
+
+    bottomContainer: {
+        flexShrink: 1,
     },
 
     titleContainer: {

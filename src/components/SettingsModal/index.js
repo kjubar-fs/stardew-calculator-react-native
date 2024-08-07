2ea14dadf8b50f8e7858eb8113e7e9d21989f05c
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-07 16:37:03 -0400

    Add global state for settings

--- src/components/SettingsModal/index.js
+++ src/components/SettingsModal/index.js
@@ -1,17 +1,31 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 7 Aug 2024, 12:27:33 PM
- *  Last update: 7 Aug 2024, 1:17:17 PM
+ *  Last update: 7 Aug 2024, 4:34:01 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Modal, View, Text } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+
+import * as settings from "../../redux/settingsSlice";
 
 import ButtonWithIcon from "../ButtonWithIcon";
-import globalStyles from "../../styles/structure";
-import styles from "./styles";
 import Checkbox from "../Checkbox";
 
+import globalStyles from "../../styles/structure";
+import styles from "./styles";
+
 export default function SettingsModal({ shown, onClose }) {
+    const rancherEnabled = useSelector((state) => state.settings.professions.rancher);
+    const gemologistEnabled = useSelector((state) => state.settings.professions.gemologist);
+    const tillerEnabled = useSelector((state) => state.settings.professions.tiller);
+    const blacksmithEnabled = useSelector((state) => state.settings.professions.blacksmith);
+    const artisanEnabled = useSelector((state) => state.settings.professions.artisan);
+    const fisherEnabled = useSelector((state) => state.settings.professions.fisher);
+    const tapperEnabled = useSelector((state) => state.settings.professions.tapper);
+    const anglerEnabled = useSelector((state) => state.settings.professions.angler);
+    const dispatch = useDispatch();
+
     return (
         <Modal
             visible={shown}
@@ -23,14 +37,70 @@ export default function SettingsModal({ shown, onClose }) {
 
                     <Text style={globalStyles.titleH2}>Professions</Text>
                     <View style={styles.checkboxGrid}>
-                        <Checkbox style={styles.checkbox} caption="Rancher" />
-                        <Checkbox style={styles.checkbox} caption="Gemologist" />
-                        <Checkbox style={styles.checkbox} caption="Tiller" />
-                        <Checkbox style={styles.checkbox} caption="Blacksmith" />
-                        <Checkbox style={styles.checkbox} caption="Artisan" />
-                        <Checkbox style={styles.checkbox} caption="Fisher" />
-                        <Checkbox style={styles.checkbox} caption="Tapper" />
-                        <Checkbox style={styles.checkbox} caption="Angler" />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Rancher"
+                            initialValue={rancherEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleRancher());
+                            }}
+                        />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Gemologist"
+                            initialValue={gemologistEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleGemologist());
+                            }}
+                        />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Tiller"
+                            initialValue={tillerEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleTiller());
+                            }}
+                        />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Blacksmith"
+                            initialValue={blacksmithEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleBlacksmith());
+                            }}
+                        />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Artisan"
+                            initialValue={artisanEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleArtisan());
+                            }}
+                        />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Fisher"
+                            initialValue={fisherEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleFisher());
+                            }}
+                        />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Tapper"
+                            initialValue={tapperEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleTapper());
+                            }}
+                        />
+                        <Checkbox
+                            style={styles.checkbox}
+                            caption="Angler"
+                            initialValue={anglerEnabled}
+                            onChange={() => {
+                                dispatch(settings.toggleAngler());
+                            }}
+                        />
                     </View>
                     
                     {/* TODO: make this a floating X button in the top right */}

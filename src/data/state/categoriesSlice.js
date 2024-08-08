871e46799b7b49154ed3fe6c00905a66803ce66d
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-08 17:57:42 -0400

    Disable unused categories, add subcategory markers

--- src/data/state/categoriesSlice.js
+++ src/data/state/categoriesSlice.js
@@ -1,16 +1,18 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 7 Aug 2024, 4:20:17 PM
- *  Last update: 8 Aug 2024, 5:08:24 PM
+ *  Last update: 8 Aug 2024, 5:57:24 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { createSlice } from "@reduxjs/toolkit";
 
+// TODO: reenable unused categories
+// TODO: rework data parser to fix subcategories
 const initialState = [
-    {
-        id: -2,
-        displayName: "Gems",
-    },
+    // {
+    //     id: -2,
+    //     displayName: "Gems",
+    // },
     {
         id: -4,
         displayName: "Fish",
@@ -18,75 +20,72 @@ const initialState = [
     {
         id: -5,
         displayName: "Eggs",
+        isSubCategory: true,
     },
     {
         id: -6,
         displayName: "Milk",
+        isSubCategory: true,
     },
     {
         id: -7,
-        displayName: "Cooking",
+        displayName: "Cooking Recipes",
     },
-    {
-        id: -8,
-        displayName: "Crafting",
-    },
-    {
-        id: -9,
-        displayName: "Big Craftables",
-    },
-    {
-        id: -12,
-        displayName: "Minerals",
-    },
-    {
-        id: -14,
-        displayName: "Meat",
-    },
-    {
-        id: -15,
-        displayName: "Metals",
-    },
-    {
-        id: -16,
-        displayName: "Building Resources",
-    },
+    // {
+    //     id: -8,
+    //     displayName: "Crafting",
+    // },
+    // {
+    //     id: -9,
+    //     displayName: "Big Craftables",
+    // },
+    // {
+    //     id: -12,
+    //     displayName: "Minerals",
+    // },
+    // {
+    //     id: -15,
+    //     displayName: "Metals",
+    // },
+    // {
+    //     id: -16,
+    //     displayName: "Building Resources",
+    // },
     {
         id: -17,
         displayName: "Sell at Pierre's",
+        isSubCategory: true,
     },
     {
         id: -18,
         displayName: "Sell at Pierre's and Marnie's",
+        isSubCategory: true,
     },
-    {
-        id: -19,
-        displayName: "Fertilizer",
-    },
-    {
-        id: -20,
-        displayName: "Junk",
-    },
-    {
-        id: -21,
-        displayName: "Bait",
-    },
-    {
-        id: -22,
-        displayName: "Tackle",
-    },
+    // {
+    //     id: -19,
+    //     displayName: "Fertilizer",
+    // },
+    // {
+    //     id: -20,
+    //     displayName: "Junk",
+    // },
+    // {
+    //     id: -21,
+    //     displayName: "Bait",
+    // },
+    // {
+    //     id: -22,
+    //     displayName: "Tackle",
+    // },
     {
         id: -23,
         displayName: "Sell at Willy's",
+        isSubCategory: true,
     },
-    {
-        id: -24,
-        displayName: "Furniture",
-    },
-    {
-        id: -25,
-        displayName: "Ingredients",
-    },
+    // {
+    //     id: -24,
+    //     displayName: "Furniture",
+    // },
     {
         id: -26,
         displayName: "Artisan Goods",
@@ -95,50 +94,53 @@ const initialState = [
         id: -27,
         displayName: "Syrup",
     },
-    {
-        id: -28,
-        displayName: "Monster Loot",
-    },
-    {
-        id: -29,
-        displayName: "Equipment",
-    },
-    {
-        id: -74,
-        displayName: "Seeds",
-    },
+    // {
+    //     id: -28,
+    //     displayName: "Monster Loot",
+    // },
+    // {
+    //     id: -29,
+    //     displayName: "Equipment",
+    // },
+    // {
+    //     id: -74,
+    //     displayName: "Seeds",
+    // },
     {
         id: -75,
         displayName: "Vegetables",
+        isSubCategory: true,
     },
     {
         id: -79,
         displayName: "Fruits",
+        isSubCategory: true,
     },
     {
         id: -80,
         displayName: "Flowers",
+        isSubCategory: true,
     },
     {
         id: -81,
-        displayName: "Greens",
+        displayName: "Forageables", // called "Greens" internally
     },
-    {
-        id: -95,
-        displayName: "Hats",
-    },
-    {
-        id: -96,
-        displayName: "Rings",
-    },
-    {
-        id: -98,
-        displayName: "Weapons",
-    },
-    {
-        id: -99,
-        displayName: "Tools",
-    },
+    // {
+    //     id: -95,
+    //     displayName: "Hats",
+    // },
+    // {
+    //     id: -96,
+    //     displayName: "Rings",
+    // },
+    // {
+    //     id: -98,
+    //     displayName: "Weapons",
+    // },
+    // {
+    //     id: -99,
+    //     displayName: "Tools",
+    // },
 ];
 
 const categoriesSlice = createSlice({

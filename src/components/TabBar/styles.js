c44adf921ae8e114ee3f56164deeb54bdefdf10b
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-08 17:29:52 -0400

    Refactor to use global style vars

--- src/components/TabBar/styles.js
+++ src/components/TabBar/styles.js
@@ -1,19 +1,19 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Aug 2024, 11:02:59 AM
- *  Last update: 6 Aug 2024, 12:05:04 PM
+ *  Last update: 8 Aug 2024, 5:21:01 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Platform, StyleSheet } from "react-native";
 
-import { darkColor, lightColor } from "../../styles/structure";
+import { darkColor, lightColor, gapPadding, screenPadding } from "../../styles/structure";
 
 export default StyleSheet.create({
     container: {
         backgroundColor: lightColor,
         flexDirection: 'row',
-        padding: 20,
-        paddingBottom: Platform.OS === "ios" ? 35 : 20,
+        padding: gapPadding,
+        paddingBottom: Platform.OS === "ios" ? screenPadding : gapPadding,
 
         borderTopColor: darkColor,
         borderTopWidth: 3,

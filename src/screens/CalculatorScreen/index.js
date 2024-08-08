12eb5817e144b3ab05400740b9f114c4f8649850
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-08 14:44:57 -0400

    Update calculator screen placeholder

--- src/screens/CalculatorScreen/index.js
+++ src/screens/CalculatorScreen/index.js
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 5 Aug 2024, 7:08:13 PM
- *  Last update: 5 Aug 2024, 8:34:07 PM
+ *  Last update: 8 Aug 2024, 2:44:42 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { View, Text } from "react-native";
@@ -11,7 +11,9 @@ import globalStyles from "../../styles/structure";
 export default function CalculatorScreen() {
     return (
         <View style={globalStyles.navContainer}>
-            <Text>Calculator screen goes here.</Text>
+            <View style={globalStyles.cardWithBorder}>
+                <Text>Calculator screen goes here.</Text>
+            </View>
         </View>
     );
 }

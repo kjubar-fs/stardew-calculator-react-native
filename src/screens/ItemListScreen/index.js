27a453c1060ed553518ff5f3c7bb723de5778562
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-09 10:32:31 -0400

    Add item list parsing

--- src/screens/ItemListScreen/index.js
+++ src/screens/ItemListScreen/index.js
@@ -1,28 +1,33 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 5 Aug 2024, 7:43:21 PM
- *  Last update: 9 Aug 2024, 10:15:47 AM
+ *  Last update: 9 Aug 2024, 10:29:16 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { ScrollView, Text, TouchableHighlight } from "react-native";
+import { ScrollView } from "react-native";
+
+import { useSelector } from "react-redux";
+
+import ButtonWithIcon from "../../components/ButtonWithIcon";
 
 import globalStyles from "../../styles/structure";
 
-export default function ItemListScreen({ navigation }) {
+export default function ItemListScreen({ navigation, route }) {
+    const allItems = useSelector((state) => state.items);
+    const itemList = Object.values(allItems).filter((item) => item.category === route.params.category.id);
+
     return (
         <ScrollView
             style={globalStyles.navContainer}
             contentContainerStyle={globalStyles.scrollContainer}
         >
-            <Text>Item list screen goes here.</Text>
-            <TouchableHighlight
-                onPress={() => navigation.navigate("ItemDetail", { itemName: "Parsnip", })}
-                underlayColor="#6AE"
-            >
-                <Text style={{ color: "#333" }}>Go to item details</Text>
-            </TouchableHighlight>
-
-
+            {itemList.map((item) => (
+                <ButtonWithIcon
+                    key={item.id}
+                    onPress={() => navigation.navigate("ItemDetail", { itemName: item.name })}
+                    caption={item.name}
+                />
+            ))}
         </ScrollView>
     );
 }
